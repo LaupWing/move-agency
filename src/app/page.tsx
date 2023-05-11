ff8fdda19bbee4372240b5557a59ab4d1f3945e5
LaupWing <loc_strict@hotmail.com>
2023-05-11 16:46:06 +0200

    udpate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image"
 export default function Home() {
    return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-         
+         <div className="p-4 bg-white rounded shadow w-full max-w-sm">
+            <h1 className="uppercase font-bold text-slate-400">
+               Tags
+            </h1>
+         </div>
       </main>
    )
 }
