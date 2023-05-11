43d96b45a11f0afbd38947f6583f815572f447d8
LaupWing <loc_strict@hotmail.com>
2023-05-11 17:56:32 +0200

    set hovering

--- src/app/components/Tags.tsx
+++ src/app/components/Tags.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { Tag } from "@/types"
-import { FC } from "react"
+import { FC, useState } from "react"
+import { GrFormClose } from "react-icons/gr"
 
 const Tags:FC<{
    tags: Tag[]
@@ -19,14 +20,24 @@ const Tags:FC<{
 export default Tags
 
 const Tag:FC<{tag: Tag}> = ({tag}) => {
+   const [hovering, setHovering] = useState(false)
+
    return (
       <div 
-         className="text-xs text-black px-2 py-1 shadow font-bold rounded"
+         className="text-xs text-black px-2 py-1 shadow font-bold rounded flex items-center"
          style={{
             backgroundColor: tag.color
          }}
+         onMouseOver={() => setHovering(true)}
+         onMouseOut={() => setHovering(false)}
       >
-         {tag.name}
+         <p>
+            {tag.name}
+         </p>
+         <GrFormClose 
+            className={`$ml-2`} 
+            size={22} 
+         />
       </div>
    )
 }
