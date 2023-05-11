24ceef7b4a309c08e59718a99dc130f6fdc310c6
LaupWing <loc_strict@hotmail.com>
2023-05-11 17:03:47 +0200

    Splitted it up

---
+++ src/app/components/FormTag.tsx
@@ -0,0 +1,27 @@
+"use client"
+import { FormEvent } from "react"
+
+export default function FormTag() {
+   const handleSubmit = (e: FormEvent) => {
+      e.preventDefault()
+      // @ts-ignore
+      const input = (e.target as HTMLFormElement).elements["tags"] as HTMLInputElement
+
+      if (input.value == "") {
+         alert("Cannot be empty")
+      } else {
+      }
+   }
+   return (
+      <form onSubmit={handleSubmit} className="w-full flex">
+         <input
+            name="tags"
+            className="rounded-l w-full border-slate-300 p-1"
+            type="text"
+         />
+         <button className="px-4 uppercase text-xs bg-indigo-400 rounded-r font-bold text-white">
+            Add
+         </button>
+      </form>
+   )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-"use client"
-import { FormEvent } from "react"
+import FormTag from "./components/FormTag"
 
 export default function Home() {
-   const handleSubmit = (e: FormEvent) => {
-      e.preventDefault()
-      // @ts-ignore
-      const value = (e.target as HTMLFormElement).elements["tags"].value
-      if(value == ""){
-         alert("Cannot be empty")
-      }
-      
-   }
+   
    return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
          <div className="p-4 bg-white rounded shadow w-full max-w-sm">
@@ -18,10 +9,7 @@ export default function Home() {
                <h1 className="uppercase font-bold text-slate-400">
                   Tags
                </h1>
-               <form onSubmit={handleSubmit} className="w-full flex">
-                  <input name="tags" className="rounded-l w-full border-slate-300 p-1" type="text" />
-                  <button className="px-4 uppercase text-xs bg-indigo-400 rounded-r font-bold text-white">Add</button>
-               </form>
+               <FormTag />
             </header>
          </div>
       </main>
