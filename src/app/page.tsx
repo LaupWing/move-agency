5d667758e5916af7d51869daf19c2b248fcbbf5c
LaupWing <loc_strict@hotmail.com>
2023-05-11 16:48:54 +0200

    udpate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
-import Image from "next/image"
 
 export default function Home() {
    return (
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
          <div className="p-4 bg-white rounded shadow w-full max-w-sm">
-            <h1 className="uppercase font-bold text-slate-400">
-               Tags
-            </h1>
+            <header className="flex flex-col">
+               <h1 className="uppercase font-bold text-slate-400">
+                  Tags
+               </h1>
+               <form className="w-full flex">
+                  <input className="rounded-l w-full border-slate-300 p-1" type="text" />
+                  <button className="px-4 uppercase text-xs bg-indigo-400 rounded-r font-bold text-white">Add</button>
+               </form>
+            </header>
          </div>
       </main>
    )
