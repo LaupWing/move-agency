3c32781e69dd00bfbe8cdc4dba5cef0e6afc6de1
LaupWing <loc_strict@hotmail.com>
2023-05-12 05:39:00 +0200

    moved

--- src/app/api/tags/route.ts
+++ src/app/api/tags/route.ts
@@ -1,7 +1,22 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient, Tag } from "@prisma/client"
 import { NextResponse } from "next/server"
 const prisma = new PrismaClient()
 
 export const GET = async () => {
    return NextResponse.json(await prisma.tag.findMany())
+}
+
+export const POST = async (request: Request) => {
+   const {tags} = await request.json()
+   
+   const promises = tags.map(async (tag: Tag) => {
+      return prisma.tag.create({
+         data: {
+            name: tag.name,
+            color: tag.color
+         }
+      })
+   })
+   const _tags = await Promise.all(promises)
+   return NextResponse.json(_tags)
 }
